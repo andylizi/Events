cf9bf6399f579a1c5547d740a81ce2fcaf3fef2b
Col-E <mcoley2@gmu.edu>
2018-10-13 11:41:28 -0400

    Update commented out test case to support java 8

--- src/test/java/me/coley/event/AccessHelperTest.java
+++ src/test/java/me/coley/event/AccessHelperTest.java
@@ -42,7 +42,7 @@ public class AccessHelperTest {
 
 		MethodHandles.Lookup newLookup = AccessHelper.narrowLookupClass(lookup, method);
 		assertEquals("lookupClass should change to MemberClassA", MemberClassA.class, newLookup.lookupClass());
-		//assertEquals("lookupModes shouldn't change", lookup.lookupModes(), newLookup.lookupModes());
+		assertTrue("lookup should have private access", (newLookup.lookupModes() & MethodHandles.Lookup.PRIVATE) != 0);
 	}
 
 	@Test
